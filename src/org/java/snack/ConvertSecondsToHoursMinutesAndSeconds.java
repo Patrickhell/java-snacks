9299f4b4feb0de7c0bc1fa76414084dc0df83c9c
Patrickhell <patrickhell19@yahoo.fr>
2023-11-13 11:16:51 +0100

    fixed break on my circle while

--- src/org/java/snack/ConvertSecondsToHoursMinutesAndSeconds.java
+++ src/org/java/snack/ConvertSecondsToHoursMinutesAndSeconds.java
@@ -11,33 +11,30 @@ public static void main(String[] args) {
 		//minuti, secondi e mostri a video la stringa generata (secondi → “hh:mm:ss”) 
 		
 	  Scanner sc = new Scanner(System.in); 
-	  
       while(true) {
     	  
 		System.out.println("Insert Seconds: ");
 		int sec = sc.nextInt();
 		
 		if(sec == 0) {
-			System.out.println("You didn't enter anything.Please insert a Number ");
-			
-		}else {
-			
-			System.out.println("Insert Seconds: ");
-			sec = sc.nextInt();
-			
-			int hour = sec / (60 * 60);
-		    int minuts = (sec / (60 * 60)) / 60;
-			int seconds = sec % 60;
-			
-			System.out.println("I convert second to Hours, Minutes and Seconds:" + 
-			  String.format("%02d", hour) + ":" + 
-			  String.format("%02d", minuts) + ":" +
-			  String.format("%02d",seconds)
-			);
+			break;
 		}
 		
+		int hour = sec / (60 * 60);
+	    int minuts = (sec / (60 * 60)) / 60;
+		int seconds = sec % 60;
+		
+		System.out.println("I convert second to Hours, Minutes and Seconds:" + 
+		  String.format("%02d", hour) + ":" + 
+		  String.format("%02d", minuts) + ":" +
+		  String.format("%02d",seconds)
+		);
+		
 	  }
+      System.out.println("You didn't enter anything ");
        
+      
+     
 	}
 
 }
